cf558454d9481f61df8fc71b2c2ca5a74bf866bd
Alexander Zakusylo <zakgof@gmail.com>
2019-09-18 22:25:27 +0300

    Basic video player

---
+++ src/main/java/com/zakgof/velvetvideo/player/MainActr.java
@@ -0,0 +1,103 @@
+package com.zakgof.velvetvideo.player;
+
+
+import java.io.File;
+
+import com.zakgof.actr.ActorRef;
+import com.zakgof.actr.ActorSystem;
+import com.zakgof.actr.Actr;
+import com.zakgof.actr.DedicatedThreadScheduler;
+import com.zakgof.velvetvideo.FFMpegVideoLib;
+import com.zakgof.velvetvideo.IVideoLib;
+import com.zakgof.velvetvideo.IVideoLib.IDemuxer;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainActr extends Application {
+
+	static ActorRef<MainActr> UI_ACTOR;
+	static ActorRef<DecoderActor> DECODER_ACTOR;
+	static ActorSystem SYSTEM = ActorSystem.create("default");
+
+	private ImageView iv;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) {
+		var params = getParameters().getRaw();
+		if (params.isEmpty()) {
+			throw new IllegalArgumentException("pass video file name via command file");
+		}
+		createActors();
+		
+		UI_ACTOR.tell(main -> main.startUI(stage));
+
+		var videoFile = params.get(0);
+		DECODER_ACTOR.tell(decoder -> decoder.startFile(videoFile));
+	}
+
+	private void createActors() {
+		DECODER_ACTOR = SYSTEM.<DecoderActor>actorBuilder()
+			.constructor(DecoderActor::new)
+			.scheduler(new DedicatedThreadScheduler("decoder"), true)
+			.build();
+		
+		UI_ACTOR = SYSTEM.<MainActr>actorBuilder()
+			.object(this)
+			.scheduler((runnable, obj) -> Platform.runLater(runnable), false)
+			.build();
+	}
+	
+	public void startUI(Stage stage) {
+		iv = new ImageView();
+		iv.fitWidthProperty().bind(stage.widthProperty());
+		iv.fitHeightProperty().bind(stage.heightProperty());
+		iv.setPreserveRatio(true);
+		Scene scene = new Scene(new StackPane(iv), 640, 480);
+		stage.setScene(scene);
+		stage.show();
+	}
+	
+	public void displayFrame(Image image) {
+		iv.setImage(image);
+	}
+
+	@Override
+	public void stop() throws Exception {
+		SYSTEM.shutdown();
+	}
+}
+
+class DecoderActor {
+	private final IVideoLib lib = new FFMpegVideoLib();
+	private long start;
+	private IDemuxer demuxer;
+	
+	void startFile(String filename) {
+		start = System.nanoTime();
+		demuxer = lib.demuxer(new File(filename));
+		decodeFrame();
+	}
+
+	private void decodeFrame() {
+		long nextFrame[] = {0};
+		if (demuxer.nextPacket(frame -> {
+			var bi = frame.image();
+			var image = SwingFXUtils.toFXImage(bi, null);
+			nextFrame[0] = start + frame.nanostamp();
+			MainActr.UI_ACTOR.tell(main -> main.displayFrame(image));
+		}, null)) {
+			Actr.<DecoderActor>current().later(DecoderActor::decodeFrame, (nextFrame[0] - System.nanoTime()) / 1000000L);
+		}
+	}
+}
---
+++ src/main/java/com/zakgof/velvetvideo/player/MainSimple.java
@@ -0,0 +1,78 @@
+package com.zakgof.velvetvideo.player;
+
+import java.io.File;
+
+import com.zakgof.velvetvideo.FFMpegVideoLib;
+
+import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainSimple extends Application {
+
+	private ImageView iv;
+	private Thread worker;
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) {
+		var params = getParameters().getRaw();
+		if (params.isEmpty()) {
+			throw new IllegalArgumentException("pass video file name via command file");
+		}
+
+		startUI(stage);
+
+		var videoFile = params.get(0);
+		worker = new Thread(() -> worker(videoFile), "decoder");
+		worker.start();
+	}
+
+	private void startUI(Stage stage) {
+		iv = new ImageView();
+		iv.fitWidthProperty().bind(stage.widthProperty());
+		iv.fitHeightProperty().bind(stage.heightProperty());
+		iv.setPreserveRatio(true);
+		Scene scene = new Scene(new StackPane(iv), 640, 480);
+		stage.setScene(scene);
+		stage.show();
+	}
+
+	@Override
+	public void stop() throws Exception {
+		worker.interrupt();
+		worker.join();
+	}
+
+	void worker(String filename) {
+		var lib = new FFMpegVideoLib();
+		try (var demuxer = lib.demuxer(new File(filename))) {
+			var start = System.nanoTime();
+			while (!Thread.currentThread().isInterrupted() && demuxer.nextPacket(frame -> {
+				var nanostamp = frame.nanostamp();
+				var bi = frame.image();
+				var image = SwingFXUtils.toFXImage(bi, null);
+				var wait = start + nanostamp - System.nanoTime();
+				if (wait > 0)
+					try {
+						Thread.sleep(wait / 1000000L, (int) (wait % 1000000L));
+					} catch (InterruptedException e) {
+						Thread.currentThread().interrupt();
+					}
+				Platform.runLater(() -> displayImage(image));
+			}, null));
+		}
+	}
+
+	void displayImage(Image image) {
+		iv.setImage(image);
+	}
+}
